8c2c405dcde5288ca03a26109ea8e94eae137813
samwachtel <sammy@wachtel.us>
2025-06-28 15:48:13 -0400

    - Add support for displaying alternate names and intervals for musical scales - Prefer standard spelling for pitch classes in non-diatonic scales - Simplify major scale interval calculation logic

--- tables.js
+++ tables.js
@@ -3,7 +3,7 @@ window.populateAllTables = function() {
 
     // --- Data and Constants ---
     const PARENT_KEYS = {
-        0: 'C', 1: 'D♭', 2: 'D', 3: 'E♭', 4: 'E', 5: 'F', 6: 'G♭', 7: 'G', 8: 'A♭', 9: 'A', 10: 'B♭', 11: 'B'
+        0: 'C', 1: 'D♭', 2: 'D', 3: 'E♭', 4: 'E', 5: 'F', 6: 'F♯', 7: 'G', 8: 'A♭', 9: 'A', 10: 'B♭', 11: 'B'
     };
     const PARENT_KEY_INDICES = [0, 7, 2, 9, 4, 11, 5, 10, 3, 8, 1, 6];
 
@@ -18,13 +18,178 @@ window.populateAllTables = function() {
     };
 
     const allScaleData = [
-        { name: "Major Scale", tableId: "major-scale-modes", isDiatonic: true, headers: ["Mode / Scale Degree", "Ionian (I)", "Dorian (II)", "Phrygian (III)", "Lydian (IV)", "Mixolydian (V)", "Aeolian (VI)", "Locrian (VII)"], formulas: ["1, 2, 3, 4, 5, 6, 7", "1, 2, ♭3, 4, 5, 6, ♭7", "1, ♭2, ♭3, 4, 5, ♭6, ♭7", "1, 2, 3, ♯4, 5, 6, 7", "1, 2, 3, 4, 5, 6, ♭7", "1, 2, ♭3, 4, 5, ♭6, ♭7", "1, ♭2, ♭3, 4, ♭5, ♭6, ♭7"], commonNames: ["Major Scale", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Natural Minor", "Locrian"], modeIntervals: [[0,2,4,5,7,9,11], [0,2,3,5,7,9,10], [0,1,3,5,7,8,10], [0,2,4,6,7,9,11], [0,2,4,5,7,9,10], [0,2,3,5,7,8,10], [0,1,3,5,6,8,10]], parentScaleIntervals: [0, 2, 4, 5, 7, 9, 11], parentScaleIntervalPattern: [2, 2, 1, 2, 2, 2] },
-        { name: "Melodic Minor", tableId: "melodic-minor-modes", isDiatonic: true, headers: ["Mode / Scale Degree", "Melodic Minor (I)", "Dorian ♭2 (II)", "Lydian Aug (III)", "Lydian Dom (IV)", "Mixo ♭6 (V)", "Locrian ♮2 (VI)", "Altered (VII)"], formulas: ["1, 2, ♭3, 4, 5, 6, 7","1, ♭2, ♭3, 4, 5, 6, ♭7","1, 2, 3, ♯4, ♯5, 6, 7","1, 2, 3, ♯4, 5, 6, ♭7","1, 2, 3, 4, 5, ♭6, ♭7","1, 2, ♭3, 4, ♭5, ♭6, ♭7","1, ♭2, ♭3, ♭4, ♭5, ♭6, ♭7"], commonNames: ["Melodic Minor", "Phrygian ♮6", "Lydian Augmented", "Lydian Dominant", "Aeolian Dominant", "Half-Diminished ♮2", "Altered Scale"], modeIntervals: [[0,2,3,5,7,9,11], [0,1,3,5,7,9,10], [0,2,4,6,8,9,11], [0,2,4,6,7,9,10], [0,2,4,5,7,8,10], [0,2,3,5,6,8,10], [0,1,3,4,6,8,10]], parentScaleIntervals: [0, 2, 3, 5, 7, 9, 11], parentScaleIntervalPattern: [2, 1, 2, 2, 2, 2] },
-        { name: "Harmonic Minor", tableId: "harmonic-minor-modes", isDiatonic: true, headers: ["Mode / Scale Degree", "Harmonic Minor (I)", "Locrian ♮6 (II)", "Ionian Aug (III)", "Dorian ♯4 (IV)", "Phrygian Dom (V)", "Lydian ♯2 (VI)", "Altered Dim (VII)"], formulas: ["1, 2, ♭3, 4, 5, ♭6, 7", "1, ♭2, ♭3, 4, ♭5, 6, ♭7", "1, 2, 3, 4, ♯5, 6, 7", "1, 2, ♭3, ♯4, 5, 6, ♭7", "1, ♭2, 3, 4, 5, ♭6, ♭7", "1, ♯2, 3, ♯4, 5, 6, 7", "1, ♭2, ♭3, ♭4, ♭5, ♭6, ♭♭7"], commonNames: ["Harmonic Minor", "Locrian ♮6", "Ionian Augmented", "Ukrainian Dorian", "Phrygian Dominant", "Lydian ♯2", "Altered Diminished"], modeIntervals: [[0,2,3,5,7,8,11], [0,1,3,5,6,8,10], [0,2,4,5,8,9,11], [0,2,3,6,7,9,10], [0,1,4,5,7,8,10], [0,3,4,7,8,10,11], [0,1,3,4,6,7,9]], parentScaleIntervals: [0, 2, 3, 5, 7, 8, 11], parentScaleIntervalPattern: [2, 1, 2, 2, 1, 3] },
-        { name: "Harmonic Major", tableId: "harmonic-major-modes", isDiatonic: true, headers: ["Mode / Scale Degree", "Harmonic Major (I)", "Dorian ♭5 (II)", "Phrygian ♭4 (III)", "Lydian ♭3 (IV)", "Mixo ♭2 (V)", "Lydian Aug ♯2 (VI)", "Locrian ♭♭7 (VII)"], formulas: ["1, 2, 3, 4, 5, ♭6, 7", "1, 2, ♭3, 4, ♭5, 6, ♭7", "1, ♭2, ♭3, ♭4, 5, ♭6, ♭7", "1, 2, ♭3, ♯4, 5, 6, 7", "1, ♭2, 3, 4, 5, 6, ♭7", "1, ♯2, 3, ♯4, ♯5, 6, 7", "1, ♭2, ♭3, 4, ♭5, ♭6, ♭♭7"], commonNames: ["Harmonic Major", "Dorian ♭5", "Phrygian ♭4", "Lydian ♭3", "Mixolydian ♭2", "Lydian Aug ♯2", "Locrian ♭♭7"], modeIntervals: [[0,2,4,5,7,8,11], [0,2,3,5,6,9,10], [0,1,3,4,7,8,10], [0,2,3,6,7,9,11], [0,1,4,5,7,9,10], [0,3,4,7,9,10,11], [0,1,3,5,6,8,9]], parentScaleIntervals: [0, 2, 4, 5, 7, 8, 11], parentScaleIntervalPattern: [2, 2, 1, 2, 1, 3] },
-        { name: "Double Harmonic Major", tableId: "double-harmonic-major-modes", isDiatonic: true, headers: ["Mode / Scale Degree", "Double Harmonic (I)", "Lydian ♯2 ♯6 (II)", "Ultraphrygian (III)", "Hungarian Minor (IV)", "Oriental (V)", "Ionian Aug ♯2 (VI)", "Locrian ♭♭3 ♭♭7 (VII)"], formulas: ["1, ♭2, 3, 4, 5, ♭6, 7", "1, ♯2, 3, ♯4, 5, ♯6, 7", "1, ♭2, ♭3, ♭4, 5, 6, ♭7", "1, 2, ♭3, ♯4, 5, ♭6, ♭7", "1, ♭2, 3, 4, ♭5, 6, ♭7", "1, ♯2, 3, 4, ♯5, 6, 7", "1, ♭2, ♭♭3, ♭4, ♭5, ♭6, ♭♭7"], commonNames: ["Byzantine Scale", "Lydian ♯2 ♯6", "Ultraphrygian", "Hungarian Minor", "Oriental", "Ionian Augmented ♯2", "Locrian ♭♭3 ♭♭7"], modeIntervals: [[0,1,4,5,7,8,11], [0,3,4,6,7,10,11], [0,1,3,4,7,9,10], [0,2,3,6,7,8,10], [0,1,4,5,6,9,10], [0,3,4,5,8,9,11], [0,1,2,4,5,7,8]], parentScaleIntervals: [0, 1, 4, 5, 7, 8, 11], parentScaleIntervalPattern: [1, 3, 1, 2, 1, 3] },
-        { name: "Major Pentatonic", tableId: "major-pentatonic-modes", isDiatonic: false, headers: ["Mode / Degree", "Major Pentatonic (I)", "Suspended (II)", "Blues Minor (III)", "Blues Major (IV)", "Minor Pentatonic (V)"], formulas: ["1, 2, 3, 5, 6", "1, 2, 4, 5, b7", "1, b3, 4, b6, b7", "1, 2, 4, 5, 6", "1, b3, 4, 5, b7"], commonNames: ["Major Pentatonic", "Egyptian", "Man Gong", "Ritsusen", "Minor Pentatonic"], modeIntervals: [[0,2,4,7,9], [0,2,5,7,10], [0,3,5,8,10], [0,2,5,7,9], [0,3,5,7,10]], parentScaleIntervals: [0, 2, 4, 7, 9] },
-        { name: "Blues Scale", tableId: "blues-scale-modes", isDiatonic: false, headers: ["Mode / Degree", "Blues Scale (I)", "Mode II", "Mode III", "Mode IV", "Mode V", "Mode VI"], formulas: ["1,♭3,4,♭5,5,♭7", "1,2,♭3,3,5,6", "1,♭2,2,4,5,♭7", "1,♭2,3,4,6,♭7", "1,2,♭3,5,6,♭7", "1,♭3,4,♭5,♭6,♭7"], commonNames: ["Blues Scale", "Blues Mode II", "Blues Mode III", "Blues Mode IV", "Blues Mode V", "Blues Mode VI"], modeIntervals: [[0,3,5,6,7,10], [0,2,3,4,7,9], [0,1,2,5,7,10], [0,1,4,5,8,10], [0,2,3,7,9,10], [0,3,5,6,8,10]], parentScaleIntervals: [0, 3, 5, 6, 7, 10] }
+      {
+        name: "Major Scale",
+        tableId: "major-scale-modes",
+        isDiatonic: true,
+        headers: ["Mode / Scale Degree", "Ionian (I)", "Dorian (II)", "Phrygian (III)", "Lydian (IV)", "Mixolydian (V)", "Aeolian (VI)", "Locrian (VII)"],
+        formulas: [
+          "1, 2, 3, 4, 5, 6, 7",
+          "1, 2, ♭3, 4, 5, 6, ♭7",
+          "1, ♭2, ♭3, 4, 5, ♭6, ♭7",
+          "1, 2, 3, ♯4, 5, 6, 7",
+          "1, 2, 3, 4, 5, 6, ♭7",
+          "1, 2, ♭3, 4, 5, ♭6, ♭7",
+          "1, ♭2, ♭3, 4, ♭5, ♭6, ♭7"
+        ],
+        commonNames: ["Ionian", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian"],
+        modeIntervals: [
+          [0,2,4,5,7,9,11],
+          [0,2,3,5,7,9,10],
+          [0,1,3,5,7,8,10],
+          [0,2,4,6,7,9,11],
+          [0,2,4,5,7,9,10],
+          [0,2,3,5,7,8,10],
+          [0,1,3,5,6,8,10]
+        ],
+        parentScaleIntervals: [0,2,4,5,7,9,11]
+      },
+      {
+        name: "Melodic Minor",
+        tableId: "melodic-minor-modes",
+        isDiatonic: true,
+        headers: ["Mode / Scale Degree", "Melodic Minor (I)", "Dorian ♭2 (II)", "Lydian Augmented (III)", "Lydian Dominant (IV)", "Mixolydian ♭6 (V)", "Locrian ♮2 (VI)", "Altered (VII)"],
+        formulas: [
+          "1, 2, ♭3, 4, 5, 6, 7",
+          "1, ♭2, ♭3, 4, 5, 6, ♭7",
+          "1, 2, 3, ♯4, ♯5, 6, 7",
+          "1, 2, 3, ♯4, 5, 6, ♭7",
+          "1, 2, 3, 4, 5, ♭6, ♭7",
+          "1, 2, ♭3, 4, ♭5, ♭6, ♭7",
+          "1, ♭2, ♭3, ♭4, ♭5, ♭6, ♭7"
+        ],
+        commonNames: ["Jazz Minor", "Dorian ♭2", "Lydian Augmented", "Lydian Dominant", "Mixolydian ♭6", "Locrian ♮2", "Altered Scale"],
+        modeIntervals: [
+          [0,2,3,5,7,9,11],
+          [0,1,3,5,7,9,10],
+          [0,2,4,6,8,9,11],
+          [0,2,4,6,7,9,10],
+          [0,2,4,5,7,8,10],
+          [0,2,3,5,6,8,10],
+          [0,1,3,4,6,8,10]
+        ],
+        parentScaleIntervals: [0,2,3,5,7,9,11]
+      },
+      {
+        name: "Harmonic Minor",
+        tableId: "harmonic-minor-modes",
+        isDiatonic: true,
+        headers: ["Mode / Scale Degree", "Harmonic Minor (I)", "Locrian ♮6 (II)", "Ionian ♯5 (III)", "Dorian ♯4 (IV)", "Phrygian Dominant (V)", "Lydian ♯2 (VI)", "Super-Locrian (VII)"],
+        formulas: [
+          "1, 2, ♭3, 4, 5, ♭6, 7",
+          "1, ♭2, ♭3, 4, ♭5, 6, ♭7",
+          "1, 2, 3, 4, ♯5, 6, 7",
+          "1, 2, ♭3, ♯4, 5, 6, ♭7",
+          "1, ♭2, 3, 4, 5, ♭6, ♭7",
+          "1, ♯2, 3, ♯4, 5, 6, 7",
+          "1, ♭2, ♭3, ♭4, ♭5, ♭6, ♭7"
+        ],
+        commonNames: ["Harmonic Minor", "Locrian ♮6", "Ionian ♯5", "Ukrainian Dorian", "Phrygian Dominant", "Lydian ♯2", "Super-Locrian"],
+        modeIntervals: [
+          [0,2,3,5,7,8,11],
+          [0,1,3,5,6,8,10],
+          [0,2,4,5,8,9,11],
+          [0,2,3,6,7,9,10],
+          [0,1,4,5,7,8,10],
+          [0,3,4,7,8,10,11],
+          [0,1,3,4,6,7,9]
+        ],
+        parentScaleIntervals: [0,2,3,5,7,8,11]
+      },
+      {
+        name: "Harmonic Major",
+        tableId: "harmonic-major-modes",
+        isDiatonic: true,
+        headers: ["Mode / Scale Degree", "Harmonic Major (I)", "Dorian ♭5 (II)", "Phrygian ♭4 (III)", "Lydian ♭3 (IV)", "Mixolydian ♭2 (V)", "Lydian Aug ♯2 (VI)", "Locrian ♭7♭5 (VII)"],
+        formulas: [
+          "1, 2, 3, 4, 5, ♭6, 7",
+          "1, 2, ♭3, 4, ♭5, 6, ♭7",
+          "1, ♭2, ♭3, ♭4, 5, ♭6, ♭7",
+          "1, 2, ♭3, ♯4, 5, 6, 7",
+          "1, ♭2, 3, 4, 5, 6, ♭7",
+          "1, ♯2, 3, ♯4, ♯5, 6, 7",
+          "1, ♭2, ♭3, 4, ♏5, ♏7"
+        ],
+        commonNames: ["Harmonic Major", "Dorian ♭5", "Phrygian ♭4", "Lydian ♭3", "Mixolydian ♭2", "Lydian Aug ♯2", "Locrian ♭7♭5"],
+        modeIntervals: [
+          [0,2,4,5,7,8,11],
+          [0,2,3,5,6,9,10],
+          [0,1,3,4,7,8,10],
+          [0,2,3,6,7,9,11],
+          [0,1,4,5,7,9,10],
+          [0,3,4,7,9,10,11],
+          [0,1,3,5,6,8,9]
+        ],
+        parentScaleIntervals: [0,2,4,5,7,8,11]
+      },
+      {
+        name: "Double Harmonic Major",
+        tableId: "double-harmonic-major-modes",
+        isDiatonic: true,
+        headers: ["Mode / Scale Degree", "Double Harmonic (I)", "Lydian ♯2 ♯6 (II)", "Ultraphrygian (III)", "Hungarian Minor (IV)", "Oriental (V)", "Ionian Aug ♯2 (VI)", "Locrian ♭♭3 ♭♭7 (VII)"],
+        formulas: [
+          "1, ♭2, 3, 4, 5, ♭6, 7",
+          "1, ♯2, 3, ♯4, 5, ♯6, 7",
+          "1, ♭2, ♭3, ♭4, 5, 6, ♭7",
+          "1, 2, ♭3, ♯4, 5, ♭6, ♭7",
+          "1, ♭2, 3, 4, ♭5, 6, ♭7",
+          "1, ♯2, 3, 4, ♯5, 6, 7",
+          "1, ♭2, ♭♭3, ♭4, ♭5, ♭6, ♭♭7"
+        ],
+        commonNames: ["Byzantine", "Lydian ♯2 ♯6", "Ultraphrygian", "Hungarian Minor", "Oriental", "Ionian Aug ♯2", "Locrian ♭♭3 ♭♭7"],
+        modeIntervals: [
+          [0,1,4,5,7,8,11],
+          [0,3,4,6,7,10,11],
+          [0,1,3,4,7,9,10],
+          [0,2,3,6,7,8,10],
+          [0,1,4,5,6,9,10],
+          [0,3,4,5,8,9,11],
+          [0,1,2,4,5,7,8]
+        ],
+        parentScaleIntervals: [0,1,4,5,7,8,11]
+      },
+      {
+        name: "Major Pentatonic",
+        tableId: "major-pentatonic-modes",
+        isDiatonic: false,
+        headers: ["Mode / Degree", "Mode I", "Mode II", "Mode III", "Mode IV", "Mode V"],
+        formulas: ["1, 2, 3, 5, 6", "1, 2, 4, 5, ♭7", "1, ♭3, 4, ♭6, ♭7", "1, 2, 4, 5, 6", "1, ♭3, 4, 5, ♭7"],
+        alternateNames: ["Major Pentatonic", "Suspended", "Jue", "Zhi", "Minor Pentatonic"],
+        modeIntervals: [
+          [0,2,4,7,9],
+          [0,2,5,7,10],
+          [0,3,5,8,10],
+          [0,2,5,7,9],
+          [0,3,5,7,10]
+        ],
+        parentScaleIntervals: [0,2,4,7,9],
+        skipCommonNames: true
+      },
+      {
+        name: "Blues Scale",
+        tableId: "blues-scale-modes",
+        isDiatonic: false,
+        headers: ["Mode / Degree", "Mode I", "Mode II", "Mode III", "Mode IV", "Mode V", "Mode VI"],
+        formulas: [
+          "1, ♭3, 4, ♭5, 5, ♭7",
+          "1, 2, ♭3, 3, 5, 6",
+          "1, ♭2, 2, 4, 5, ♭7",
+          "1, ♭2, 3, ♯4, 6, 7",
+          "1, ♭3, 4, ♭6, ♭7, 7",
+          "1, 2, ♭3, 5, ♭6, 6"
+        ],
+        commonNames: ["Blues Scale", "Blues Mode II", "Blues Mode III", "Blues Mode IV", "Blues Mode V", "Blues Mode VI"],
+        modeIntervals: [
+          [0, 3, 5, 6, 7, 10],
+          [0, 2, 3, 4, 7, 9],
+          [0, 1, 2, 5, 7, 10],
+          [0, 1, 4, 6, 9, 11],
+          [0, 3, 5, 8, 10, 11],
+          [0, 2, 3, 7, 8, 9]
+        ],
+        parentScaleIntervals: [0,3,5,6,7,10],
+        skipCommonNames: true
+      }
     ];
 
     let allScales = [];
@@ -74,10 +239,24 @@ window.populateAllTables = function() {
             const cell = formulaRow.insertCell();
             cell.innerHTML = `<code>(${formulaText})</code>`;
         });
-        const commonNameRow = tbody.insertRow();
-        commonNameRow.insertCell().textContent = "Common Name";
-        data.commonNames.forEach(name => {
-            commonNameRow.insertCell().textContent = name;
+        if (!data.skipCommonNames) {
+            const commonNameRow = tbody.insertRow();
+            commonNameRow.insertCell().textContent = "Common Name";
+            data.commonNames.forEach(name => {
+                commonNameRow.insertCell().textContent = name;
+            });
+        }
+        if (data.alternateNames) {
+            const altNameRow = tbody.insertRow();
+            altNameRow.insertCell().textContent = "Alternate Names";
+            data.alternateNames.forEach(name => {
+                altNameRow.insertCell().textContent = name;
+            });
+        }
+        const intervalRow = tbody.insertRow();
+        intervalRow.insertCell().textContent = "Intervals";
+        data.modeIntervals.forEach(intervals => {
+            intervalRow.insertCell().textContent = intervals.join(", ");
         });
 
         PARENT_KEY_INDICES.forEach((parentKeyIndex, keyRowIndex) => {
@@ -97,15 +276,23 @@ window.populateAllTables = function() {
                 if (data.isDiatonic) {
                     cellNoteText = diatonicParent[modeIndex];
                 } else {
-                    const majorScaleIntervals = [0, 2, 4, 5, 7, 9, 11];
-                    for(let i=0; i < majorScaleIntervals.length; i++){
-                        if( (parentKeyIndex + majorScaleIntervals[i]) % 12 === modeRootPitch) {
-                            cellNoteText = diatonicParent[i];
-                            break;
-                        }
+                    // For non-diatonic scales, prefer the normal spelling; fallback to single flat or single sharp
+                    const pitchEntry = PITCH_CLASS_NAMES[modeRootPitch];
+                    // Use normal name if it doesn't contain double accidentals
+                    if (pitchEntry.normal && !pitchEntry.normal.includes('♭♭') && !pitchEntry.normal.includes('♯♯')) {
+                        cellNoteText = pitchEntry.normal;
                     }
-                    if (cellNoteText === '?') {
-                        cellNoteText = PITCH_CLASS_NAMES[modeRootPitch].flat || PITCH_CLASS_NAMES[modeRootPitch].normal;
+                    // Otherwise try flat spelling if it's a single flat
+                    else if (pitchEntry.flat && !pitchEntry.flat.includes('♭♭')) {
+                        cellNoteText = pitchEntry.flat;
+                    }
+                    // Otherwise try sharp spelling if it's a single sharp
+                    else if (pitchEntry.sharp && !pitchEntry.sharp.includes('♯♯')) {
+                        cellNoteText = pitchEntry.sharp;
+                    }
+                    // Fallback to normal if all else fails
+                    else {
+                        cellNoteText = pitchEntry.normal;
                     }
                 }
 
